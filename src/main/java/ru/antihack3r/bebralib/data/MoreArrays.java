0f74ea380745b35e4b266d1fb6427268f49451cf
rusnuker <49472785+rusnuker@users.noreply.github.com>
2024-05-02 16:27:05 +0300

    some more of them javadox

--- src/main/java/ru/antihack3r/bebralib/data/MoreArrays.java
+++ src/main/java/ru/antihack3r/bebralib/data/MoreArrays.java
@@ -18,9 +18,9 @@ import java.util.stream.Collectors;
 public class MoreArrays {
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(byte[] array, Consumer<Byte> action) {
 		for (byte _byte: array)
@@ -28,9 +28,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(short[] array, Consumer<Short> action) {
 		for (short _short: array)
@@ -38,9 +38,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(int[] array, Consumer<Integer> action) {
 		for (int _int: array)
@@ -48,9 +48,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(long[] array, Consumer<Long> action) {
 		for (long _long: array)
@@ -58,9 +58,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(float[] array, Consumer<Float> action) {
 		for (float _float: array)
@@ -68,9 +68,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(double[] array, Consumer<Double> action) {
 		for (double _double: array)
@@ -78,9 +78,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(boolean[] array, Consumer<Boolean> action) {
 		for (boolean _boolean: array)
@@ -88,9 +88,9 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static void forEach(char[] array, Consumer<Character> action) {
 		for (char _char: array)
@@ -98,105 +98,219 @@ public class MoreArrays {
 	}
 	
 	/**
-	 * Performs an {@code action} for each component of an {@code array}.
-	 * @param array An array.
-	 * @param action An action.
+	 * Performs an action for each component of a given array.
+	 * @param array An array whose elements should be filtered.
+	 * @param action An action that should be performed on each component of a given array.
 	 */
 	public static <T> void forEach(T[] array, Consumer<? super T> action) {
 		for (T thing: array)
 			action.accept(thing);
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static byte[] filter(byte[] array, Predicate<Byte> predicate) {
 		return asByteArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static short[] filter(short[] array, Predicate<Short> predicate) {
 		return asShortArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static int[] filter(int[] array, Predicate<Integer> predicate) {
 		return asIntArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static long[] filter(long[] array, Predicate<Long> predicate) {
 		return asLongArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static float[] filter(float[] array, Predicate<Float> predicate) {
 		return asFloatArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static double[] filter(double[] array, Predicate<Double> predicate) {
 		return asDoubleArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static boolean[] filter(boolean[] array, Predicate<Boolean> predicate) {
 		return asBooleanArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static char[] filter(char[] array, Predicate<Character> predicate) {
 		return asCharArray(asList(array).stream().filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Filters the given array with a given predicate.
+	 * @param array an array that should be filtered.
+	 * @param predicate a predicate that should be used.
+	 * @return a filtered array.
+	 */
 	public static <T> T[] filter(T[] array, Predicate<? super T> predicate) {
 		return asArray(Arrays.stream(array).filter(predicate).collect(Collectors.toList()));
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Byte> asList(byte[] array) {
 		ArrayList<Byte> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Short> asList(short[] array) {
 		ArrayList<Short> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Integer> asList(int[] array) {
 		ArrayList<Integer> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Long> asList(long[] array) {
 		ArrayList<Long> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Float> asList(float[] array) {
 		ArrayList<Float> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Double> asList(double[] array) {
 		ArrayList<Double> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Boolean> asList(boolean[] array) {
 		ArrayList<Boolean> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static List<Character> asList(char[] array) {
 		ArrayList<Character> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements of the given array to a new {@link ArrayList}.
+	 * @param array an array whose elements should be added to a new {@link ArrayList}.
+	 * @return an {@link ArrayList} containing the elements of the given array in the order
+	 * that they are present in the array.
+	 */
 	public static <T> List<T> asList(T[] array) {
 		ArrayList<T> arrayList = new ArrayList<>(array.length);
 		forEach(array, arrayList::add);
 		return arrayList;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static byte[] asByteArray(List<Byte> list) {
 		byte[] newArray = new byte[list.size()];
 		int counter = 0;
@@ -208,6 +322,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static short[] asShortArray(List<Short> list) {
 		short[] newArray = new short[list.size()];
 		int counter = 0;
@@ -219,6 +339,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static int[] asIntArray(List<Integer> list) {
 		int[] newArray = new int[list.size()];
 		int counter = 0;
@@ -230,6 +356,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static long[] asLongArray(List<Long> list) {
 		long[] newArray = new long[list.size()];
 		int counter = 0;
@@ -241,6 +373,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static float[] asFloatArray(List<Float> list) {
 		float[] newArray = new float[list.size()];
 		int counter = 0;
@@ -252,6 +390,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static double[] asDoubleArray(List<Double> list) {
 		double[] newArray = new double[list.size()];
 		int counter = 0;
@@ -263,6 +407,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static boolean[] asBooleanArray(List<Boolean> list) {
 		boolean[] newArray = new boolean[list.size()];
 		int counter = 0;
@@ -274,6 +424,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static char[] asCharArray(List<Character> list) {
 		char[] newArray = new char[list.size()];
 		int counter = 0;
@@ -285,6 +441,12 @@ public class MoreArrays {
 		return newArray;
 	}
 	
+	/**
+	 * Adds elements if the given {@link List} to a new array.
+	 * @param list a {@link List} which elements should be added to a new array.
+	 * @return a new array containing the elements of the given {@link List} in the order
+	 * that they are present in the list.
+	 */
 	public static <T> T[] asArray(List<T> list) {
 		return list.stream().toArray((size) -> newArray(list.getClass().getComponentType(), size));
 	}
