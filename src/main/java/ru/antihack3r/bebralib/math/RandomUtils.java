e7221c14a49d4da4215747d77c266920abdfb615
rusnuker <49472785+rusnuker@users.noreply.github.com>
2024-05-05 17:13:16 +0300

    yes javadoc (now) (and some more stuff)

--- src/main/java/ru/antihack3r/bebralib/math/RandomUtils.java
+++ src/main/java/ru/antihack3r/bebralib/math/RandomUtils.java
@@ -4,24 +4,75 @@
 
 package ru.antihack3r.bebralib.math;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Random;
 
+/**
+ * Produces randomness.
+ */
 public class RandomUtils {
 	
-	public static Random RANDOM = new Random();
+	/**
+	 * RNG used.
+	 */
+	private static Random RANDOM = new Random();
 	
+	/**
+	 * Resets the RNG used by this class.
+	 */
 	public static void resetRandom() {
 		RANDOM = new Random();
 	}
 	
-	public static int nextInt(int min, int max) {
-		return RANDOM.nextInt(max - min) + min;
+	/**
+	 * Sets the specified RNG for use in this class.
+	 * @param newRandom a new RNG.
+	 */
+	public static void setRandom(@Nullable Random newRandom) {
+		if (newRandom == null)
+			resetRandom();
+		else
+			RANDOM = newRandom;
 	}
 	
+	/**
+	 * Gets the current RNG used by this class.
+	 * @return the current RNG used by this class.
+	 */
+	public static @Nonnull Random getRandom() {
+		return RANDOM;
+	}
+	
+	/**
+	 * Generates a random <tt>int</tt> value between <tt>min</tt> and <tt>max</tt>, all-inclusive.
+	 * @param min the lower bound, inclusive.
+	 * @param max the upper bound, inclusive.
+	 * @return a random <tt>int</tt> value between <tt>min</tt> and <tt>max</tt>.
+	 * @see Random#nextInt(int)
+	 */
+	public static int nextInt(int min, int max) {
+		return RANDOM.nextInt(max - min + 1) + min;
+	}
+	
+	/**
+	 * Generates a random <tt>double</tt> value between <tt>min</tt> (inclusive) and <tt>max</tt> (exclusive).
+	 * @param min the lower bound, inclusive.
+	 * @param max the upper bound, exclusive.
+	 * @return a random <tt>double</tt> value between <tt>min</tt> (inclusive) and <tt>max</tt> (exclusive).
+	 * @see Random#nextDouble()
+	 */
 	public static double nextDouble(double min, double max) {
 		return Meth.interpolate(min, max, RANDOM.nextDouble());
 	}
 	
+	/**
+	 * Generates a random <tt>float</tt> value between <tt>min</tt> (inclusive) and <tt>max</tt> (exclusive).
+	 * @param min the lower bound, inclusive.
+	 * @param max the upper bound, exclusive.
+	 * @return a random <tt>float</tt> value between <tt>min</tt> (inclusive) and <tt>max</tt> (exclusive).
+	 * @see Random#nextFloat()
+	 */
 	public static float nextFloat(float min, float max) {
 		return Meth.interpolate(min, max, RANDOM.nextFloat());
 	}
