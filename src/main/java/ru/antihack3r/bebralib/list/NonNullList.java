95993d58eb8f99f3511ff5ad619aa57711dd3f6c
rusnuker <49472785+rusnuker@users.noreply.github.com>
2024-05-06 16:56:47 +0300

    wtfpl ftw

--- src/main/java/ru/antihack3r/bebralib/list/NonNullList.java
+++ src/main/java/ru/antihack3r/bebralib/list/NonNullList.java
@@ -1,5 +1,10 @@
 /*
  * This file is a part of bebralib -- you can get yo copy at https://github.com/antihack3r/bebralib
+ *
+ * This code is free software. It comes without any warranty, to the extent permitted by
+ * applicable law. You can redistribute it and/or modify it under the terms of the
+ * Do What The Fuck You Want To Public License, Version 2, as published by Sam Hocevar.
+ * See http://www.wtfpl.net/ for more details.
  */
 
 package ru.antihack3r.bebralib.list;
